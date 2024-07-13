a02ae420cdf08a8d725bc136d6cb70f4268153c2
linhrau <linhntph45998@fpt.edu.vn>
2024-07-13 16:30:29 +0700

    hình ảnh

---
+++ view/header.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Home-8 || Uniqlo-Minimalist eCommerce Bootstrap 5 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="apple-touch-icon.html">
+
+    <!-- Bootstrap Fremwork Main Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- All Plugins css -->
+    <link rel="stylesheet" href="css/plugins.css">
+    <!-- Theme shortcodes/elements style -->
+    <link rel="stylesheet" href="css/shortcode/shortcodes.css">
+    <!-- Theme main style -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- User style -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- Modernizr JS -->
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+</head>
+
+<body>
+    <!-- Body main wrapper start -->
+    <div class="wrapper fixed__footer home-8">
+        <!-- Start Header Style -->
+        <header id="header" class="htc-header">
+            <!-- Start Mainmenu Area -->
+            <div id="sticky-header-with-topbar" class="mainmenu__area sticky__header">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-2 col-lg-2 col-6">
+                            <div class="logo">
+                                <a href="index.html">
+                                    <img src="images/logo/uniqlo.png" alt="logo">
+                                </a>
+                            </div>
+                        </div>
+                        <!-- Start MAinmenu Ares -->
+                        <div class="col-md-8 col-lg-8 d-none d-md-block">
+                            <nav class="mainmenu__nav  d-none d-lg-block">
+                                <ul class="main__menu">
+                                    <li class="drop"><a href="index.html">Home</a>
+                                        <ul class="dropdown">
+                                            <li><a href="index.html">Home 1</a></li>
+                                            <li><a href="index-2.html">Home 2</a></li>
+                                            <li><a href="index-3.html">Home 3</a></li>
+                                            <li><a href="index-4.html">Home 4</a></li>
+                                            <li><a href="index-5.html">Home 5</a></li>
+                                            <li><a href="index-6.html">Home 6</a></li>
+                                            <li><a href="index-7.html">Home 7</a></li>
+                                            <li><a href="index-8.html">Home 8</a></li>
+                                            <li><a href="index-9.html">Home 9</a></li>
+                                            <li><a href="index-10.html">Home 10</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="about.html">About</a></li>
+                                    <li class="drop"><a href="blog.html">Blog</a>
+                                        <ul class="dropdown">
+                                            <li><a href="blog.html">blog</a></li>
+                                            <li><a href="blog-details.html">blog details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="drop"><a href="shop.html">Shop</a>
+                                        <ul class="dropdown mega_dropdown">
+                                            <!-- Start Single Mega MEnu -->
+                                            <li><a class="mega__title" href="shop.html">Shop Pages</a>
+                                                <ul class="mega__item">
+                                                    <li><a href="shop.html">shop</a></li>
+                                                    <li><a href="shop-sidebar.html">shop sidebar</a></li>
+                                                    <li><a href="cart.html">cart</a></li>
+                                                    <li><a href="checkout.html">checkout</a></li>
+                                                    <li><a href="wishlist.html">wishlist</a></li>
+                                                </ul>
+                                            </li>
+                                            <!-- End Single Mega MEnu -->
+                                            <!-- Start Single Mega MEnu -->
+                                            <li><a class="mega__title" href="shop.html">Product Types</a>
+                                                <ul class="mega__item">
+                                                    <li><a href="product-details.html">Simple Product</a></li>
+                                                    <li><a href="product-details.html">Variable Product</a></li>
+                                                    <li><a href="product-details.html">Grouped Product</a></li>
+                                                    <li><a href="product-details.html">Downloadable Product</a></li>
+                                                    <li><a href="product-details.html">Simple Product</a></li>
+                                                </ul>
+                                            </li>
+                                            <!-- End Single Mega MEnu -->
+                                        </ul>
+                                    </li>
+                                    <li class="drop"><a href="#">pages</a>
+                                        <ul class="dropdown">
+                                            <li><a href="about.html">about</a></li>
+                                            <li><a href="shop.html">shop</a></li>
+                                            <li><a href="shop-sidebar.html">shop sidebar</a></li>
+                                            <li><a href="product-details.html">product details</a></li>
+                                            <li><a href="cart.html">cart</a></li>
+                                            <li><a href="wishlist.html">wishlist</a></li>
+                                            <li><a href="checkout.html">checkout</a></li>
+                                            <li><a href="team.html">team</a></li>
+                                            <li><a href="login-register.html">login & register</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">contact</a></li>
+                                </ul>
+                            </nav>
+                            <div class="mobile-menu clearfix d-block d-lg-none">
+                                <nav id="mobile_dropdown">
+                                    <ul>
+                                        <li><a href="index.html">Home</a>
+                                            <ul>
+                                                <li><a href="index.html">Home 1</a></li>
+                                                <li><a href="index-2.html">Home 2</a></li>
+                                                <li><a href="index-3.html">Home 3</a></li>
+                                                <li><a href="index-4.html">Home 4</a></li>
+                                                <li><a href="index-5.html">Home 5</a></li>
+                                                <li><a href="index-6.html">Home 6</a></li>
+                                                <li><a href="index-7.html">Home 7</a></li>
+                                                <li><a href="index-8.html">Home 8</a></li>
+                                                <li><a href="index-9.html">Home 9</a></li>
+                                                <li><a href="index-10.html">Home 10</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="blog.html">blog</a>
+                                            <ul>
+                                                <li><a href="blog.html">blog</a></li>
+                                                <li><a href="blog-details.html">blog details</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">pages</a>
+                                            <ul>
+                                                <li><a href="about.html">about</a></li>
+                                                <li><a href="shop.html">shop</a></li>
+                                                <li><a href="shop-sidebar.html">shop sidebar</a></li>
+                                                <li><a href="product-details.html">product details</a></li>
+                                                <li><a href="cart.html">cart</a></li>
+                                                <li><a href="wishlist.html">wishlist</a></li>
+                                                <li><a href="checkout.html">checkout</a></li>
+                                                <li><a href="team.html">team</a></li>
+                                                <li><a href="login-register.html">login & register</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.html">contact</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <!-- End MAinmenu Ares -->
+                        <div class="col-md-2 col-lg-2 col-6">
+                            <ul class="menu-extra">
+                                <li class="search search__open d-none d-sm-block"><span class="ti-search"></span></li>
+                                <li><a href="login-register.html"><span class="ti-user"></span></a></li>
+                                <li class="cart__menu"><span class="ti-shopping-cart"></span></li>
+                                <li class="toggle__menu  d-none d-md-block"><span class="ti-menu"></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="mobile-menu-area"></div>
+                </div>
+            </div>
+            <!-- End Mainmenu Area -->
+        </header>
+        <!-- End Header Style -->
